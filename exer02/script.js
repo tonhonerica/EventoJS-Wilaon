e0ef675b64db788ba94351fdf87ab283e7c6959a
tonhonerica <tonhoner@gmail.com>
2025-08-27 22:46:01 -0300

    imagem

---
+++ exer02/script.js
@@ -0,0 +1,8 @@
+const luz = document.getElementById('luz');
+const mensagem = document.getElementById('mensagem');
+imagem.addEventListener('mouseover', function() {
+mensagem.textContent ='Passou mouse na imagem' 
+mensagem.addEventListener('mouseout', function(){mensagem.textContent =''
+
+});
+});
